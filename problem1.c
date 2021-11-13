047905dda32c8616bf3f6dc915a01b9365617504
justinsoberano <jsoberano2002@gmail.com>
2021-11-13 15:40:28 -0600

    Quiz 09

--- problem1.c
+++ problem1.c
@@ -9,22 +9,53 @@ struct student {
     double examScore3;
 };
 
+double examAverage(double exam1, double exam2, double exam3);
+
 int main () {
 
-    FILE* studentExam = NULL;
+    FILE* studentExam;
     studentExam = fopen("student_data.txt", "w");
     struct student record[4];
+    double studentExamAverage;
+    int i;
 
-    for (int i = 0; i < 4; i++) {
+    for (i = 0; i < 4; i++) {
         printf("Enter student %d and three exam scores: ", (i+1));
         scanf("%s %lf %lf %lf", record[i].name, &record[i].examScore1, 
                                 &record[i].examScore2, &record[i].examScore3);
         fprintf(studentExam, "%s %lf %lf %lf\n", record[i].name, record[i].examScore1, 
-                                record[i].examScore2, record[i].examScore3);
+                                                 record[i].examScore2, record[i].examScore3);
     }
     fclose(studentExam);
 
+    studentExam = fopen("student_data.txt", "r");
+    for (i = 0; i < 4; i++) {
+        fscanf(studentExam, "%s %lf %lf %lf", record[i].name, &record[i].examScore1, 
+                                &record[i].examScore2, &record[i].examScore3);
+        
+        studentExamAverage = examAverage(record[i].examScore1, record[i].examScore2, record[i].examScore3);
+
+        printf("%s exam scores = %.2lf, %.2lf, %.2lf\n", record[i].name, record[i].examScore1, 
+                                                         record[i].examScore2, record[i].examScore3);
+
+    }
+    
+    for (i = 0; i < 4; i++) {
+        fscanf(studentExam, "%s %lf %lf %lf", record[i].name, &record[i].examScore1, 
+                                &record[i].examScore2, &record[i].examScore3);
+
+        studentExamAverage = examAverage(record[i].examScore1, record[i].examScore2, record[i].examScore3);
+
+        printf("%s exam average = %.2lf\n", record[i].name, studentExamAverage);
+
+    }
 
 
+}
+
+
+double examAverage(double exam1, double exam2, double exam3) {
+
+    return ((exam1 + exam2 + exam3) / 3);
 
 }
