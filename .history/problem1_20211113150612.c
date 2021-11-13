eb9185a1f57f8f18dfe9aea49c71a404a175a194
justinsoberano <jsoberano2002@gmail.com>
2021-11-13 15:17:07 -0600

    History added

---
+++ .history/problem1_20211113150612.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct student {
+    char name[20];
+    double examScore1;
+    double examScore2;
+    double examScore3;
+};
+
+int main () {
+
+    FILE* studentExam = NULL;
+    studentExam = fopen("student_data.txt", "w");
+    struct student record[4];
+
+    for (int i = 0; i < 4; i++) {
+        printf("Enter student %d and three exam scores: ", (i +1));
+        scanf("%s %ld %lf %lf", record[i].name, record[i].examScore1, 
+                                record[i].examScore2, record[i].examScore3);
+    }
+
+
+
+
+}
